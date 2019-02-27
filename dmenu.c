3034d2dca070e823bcaafd7915e1420e3d43abc3
astier <aleksandrs.stier@uni-bielefeld.de>
2019-02-27 21:44:55 +0100

    Add an option which defines the lineheight
    Despite both the panel and dmenu using the same font (a Terminus 12),
    dmenu is shorter and the panel is visible from under the dmenu bar.
    The appearance can be even more distracting when using similar colors
    for background and selections. With the option added by this patch,
    dmenu can be launched with a '-h 24', thus completely covering the panel.
    
    Signed-off-by: Miles Alan <m@milesalan.com>

--- dmenu.c
+++ dmenu.c
@@ -608,6 +608,7 @@ setup(void)
 
 	/* calculate menu geometry */
 	bh = drw->fonts->h + 2;
+	bh = MAX(bh,lineheight);	/* make a menu line AT LEAST 'lineheight' tall */
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
 #ifdef XINERAMA
@@ -722,6 +723,10 @@ main(int argc, char *argv[])
 			prompt = argv[++i];
 		else if (!strcmp(argv[i], "-fn"))  /* font or font set */
 			fonts[0] = argv[++i];
+		else if(!strcmp(argv[i], "-h")) { /* minimum height of one menu line */
+			lineheight = atoi(argv[++i]);
+			lineheight = MAX(lineheight,8); /* reasonable default in case of value too small/negative */
+		}
 		else if (!strcmp(argv[i], "-nb"))  /* normal background color */
 			colors[SchemeNorm][ColBg] = argv[++i];
 		else if (!strcmp(argv[i], "-nf"))  /* normal foreground color */
